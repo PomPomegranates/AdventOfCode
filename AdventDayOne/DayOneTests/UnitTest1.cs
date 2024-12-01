c2058fa3112ff7ae00777d1b14f744ae9c129c56
PomPomegranates <persephoneapplewhite@gmail.com>
2024-12-01 19:08:29 +0000

    Added Day One

---
+++ AdventDayOne/DayOneTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DayOneTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
