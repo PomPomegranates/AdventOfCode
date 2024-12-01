486e8024f5d9c9257feea0c0646014bafe7637eb
PomPomegranates <persephoneapplewhite@gmail.com>
2024-12-01 20:15:36 +0000

    Part One Completed

---
+++ AdventDayOne/DayOne/PartOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace DayOne
+{
+    public static class PartOne
+    {
+       public static void PartOneComplete()
+        {
+
+            string[] wholeFile = File.ReadAllLines("./input.txt");
+            var leftSide = new List<int>();
+            var rightSide = new List<int>();
+            foreach (string part in wholeFile)
+            {
+                leftSide.Add(int.Parse(part.Split("   ")[0]));
+                rightSide.Add(int.Parse(part.Split("   ")[1]));
+            }
+            var orderedLeft = leftSide.Order().ToList();
+            var orderedRight = rightSide.Order().ToList();
+            var differences = new List<int>();
+            for (int i = 0; i < orderedLeft.Count; i++)
+            {
+                if (orderedLeft[i]> orderedRight[i])
+                {
+                    differences.Add(orderedLeft[i] - orderedRight[i]);
+                }
+                else
+                {
+                    differences.Add(orderedRight[i] - orderedLeft[i]);
+                }
+            }
+            Console.WriteLine(differences.Sum());
+        }
+    }
+}
--- AdventDayOne/DayOne/Program.cs
+++ AdventDayOne/DayOne/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PartOne.PartOneComplete();
+            //Listed by LocationID. 
+            // Pair up the numbers. Smallest to smallest. 
+            
         }
     }
 }
