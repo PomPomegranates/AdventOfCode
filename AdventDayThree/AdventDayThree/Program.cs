cf61a658b2d8e94f69d189c960e7e0ce6823bd5c
PomPomegranates <persephoneapplewhite@gmail.com>
2024-12-11 14:35:19 +0000

    Added Day Three

---
+++ AdventDayThree/AdventDayThree/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventDayThree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //(mul\(\d{ 1,3},\d{ 1,3}\))
+        }
+    }
+}
---
+++ AdventDayThree/AdventOfCodeDay3Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCodeDay3Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
